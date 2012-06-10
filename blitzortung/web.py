f9f225db0456de57b9fa6d24842adf7db6f7e531
Andreas Wuerl <andi@debian>
2012-06-11 00:31:44 +0200

    use better error handling of urllib2

--- blitzortung/web.py
+++ blitzortung/web.py
@@ -4,34 +4,40 @@
 
 '''
 
-import urllib
+import sys
+import urllib2
 
 import builder
 
 class Url(object):
 
-    base = 'blitzortung.net/Data_%d/Protected/'
+    host = 'http://blitzortung.net'
+    base = host + '/Data_%d/Protected/'
 
     def __init__(self, baseurl):
         self.url = baseurl
-        self.protocol = 'http'
-        self.credentials = ''
-
-    def add(self, name, value):
-        self.url += '&' + str(name).strip() + '=' + str(value).strip()
+        self.username = ''
+        self.password = ''
 
     def set_credentials(self, username, password):
-        self.credentials = username + ':' + password + '@'
-
-    def get(self):
-        return self.protocol + '://' + self.credentials + self.url
+        self.username = username
+	self.password = password
 
     def read(self):
-        urlconnection = urllib.urlopen(self.get())
+	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	password_mgr.add_password("Blitzortung.org", "http://blitzortung.net", self.username, self.password)
+	handler = urllib2.HTTPBasicAuthHandler(password_mgr)
 
-        data = urlconnection.read().strip()
+	opener = urllib2.build_opener(handler)
 
-        urlconnection.close()
+  	try:
+	    urlconnection = opener.open(self.url)
+        except urllib2.HTTPError, e:
+	    sys.stderr.write("%s when opening '%s'\n" %(e, self.url))
+	    return None
+
+	data = urlconnection.read().strip()
+	urlconnection.close()
 
         return data.decode('ISO-8859-1')
     
@@ -67,4 +73,10 @@ class Stations(Url):
 
     def __init__(self, config, region=1):
         super(Stations, self).__init__(Url.base %(region) + 'stations.txt')
+        self.set_credentials(config.get('USERNAME'), config.get('PASSWORD'))
+
+class Raw(Url):
+
+    def __init__(self, config, region, station_id, offset=0):
+        super(Raw, self).__init__(Url.base %(region) + 'raw_data/%s/%02d.log' %(station_id, offset))
         self.set_credentials(config.get('USERNAME'), config.get('PASSWORD'))
