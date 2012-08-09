99c9b1facecf80d9cb187ec06734d8c4eb6627da
Andreas Wuerl <andi@h1425961.stratoserver.net>
2012-08-09 15:03:27 +0200

    deactivated plot test

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -8,24 +8,24 @@ import matplotlib as plt
 
 import blitzortung
 
-class Plot(blitzortung.plot.Plot):
-    
-    def plot(self, *args):
-        x = np.arange(-5, 5, 0.1)
-        y = np.sin(x)
-        
-        plot = self.figure.add_subplot(111)
-        plot.plot(x,y)
-        
-class PlotTest(unittest.TestCase):
-    
-    def test_plot_generation(self):
-        plot = Plot()
-        
-        picture = png.Reader(bytes=plot.read())
-        
-        (width, height, pixels, metadata) = picture.read()
-        
-        self.assertEqual(480, width)
-        self.assertEqual(320, height)
-        self.assertEqual(8, metadata['bitdepth'])
+#class Plot(blitzortung.plot.Plot):
+#    
+#    def plot(self, *args):
+#        x = np.arange(-5, 5, 0.1)
+#        y = np.sin(x)
+#        
+#        plot = self.figure.add_subplot(111)
+#        plot.plot(x,y)
+#        
+#class PlotTest(unittest.TestCase):
+#    
+#    def test_plot_generation(self):
+#        plot = Plot()
+#        
+#        picture = png.Reader(bytes=plot.read())
+#        
+#        (width, height, pixels, metadata) = picture.read()
+#        
+#        self.assertEqual(480, width)
+#        self.assertEqual(320, height)
+#        self.assertEqual(8, metadata['bitdepth'])
