f7e0d7d05654dfcef167d2cb4289ddbc6b1e7999
Andreas Würl <andi@tryb.de>
2013-09-29 22:04:46 +0200

    added test for end time at current time

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,6 +2,7 @@ import unittest
 import math
 import datetime
 from hamcrest import assert_that, contains
+import pytz
 
 import blitzortung
 
@@ -12,6 +13,7 @@ class TimeIntervalsTest(unittest.TestCase):
 
     def initialize_times(self, end_time):
         self.end_time = end_time
+        end_time = end_time.replace(tzinfo=pytz.UTC)
         self.start_time = end_time - datetime.timedelta(minutes=25)
 
     def test_time_intervals_generator(self):
@@ -47,3 +49,15 @@ class TimeIntervalsTest(unittest.TestCase):
             datetime.datetime(2013, 8, 20, 11, 50, 0),
             datetime.datetime(2013, 8, 20, 12,  0, 0),
             ))
+
+    def test_time_intervals_generator_at_current_time(self):
+        end_time = datetime.datetime.utcnow()
+        end_time = end_time.replace(tzinfo=pytz.UTC)
+        start_time = end_time - self.duration
+
+        times = [time for time in blitzortung.util.time_intervals(start_time, self.duration)]
+
+        assert_that(times, contains(
+            blitzortung.util.round_time(start_time, self.duration),
+            blitzortung.util.round_time(end_time, self.duration)
+        ))
