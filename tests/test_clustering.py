e44176f5540c1df1ec14567ec20a1afa7a703ef4
Andreas Würl <andi@tryb.de>
2014-08-31 10:38:13 +0200

    fixed test

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -17,7 +17,6 @@ from hamcrest import assert_that, is_, close_to
 
 import nose
 import numpy as np
-import scipy.spatial.distance
 
 try:
     import fastcluster
@@ -66,8 +65,6 @@ class TestClustering(TestCase):
         data = np.array(data)
 
         dist = fastcluster.pdist(data)
-        other_dist = blitzortung.clustering.pdist(data)
-        dist_square = scipy.spatial.distance.squareform(dist)
         result = fastcluster.linkage(dist).tolist()
 
         assert_that(int(result[0][0]), is_(0))
