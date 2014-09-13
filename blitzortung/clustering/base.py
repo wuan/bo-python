4e9d2cf83ffac2d911f9a689b0f293a5788c2267
Andreas Wuerl <andi@tryb.de>
2014-09-13 16:00:29 +0000

    updated distance limit

--- blitzortung/clustering/base.py
+++ blitzortung/clustering/base.py
@@ -25,7 +25,7 @@ from .pdist import pdist
 
 
 class Clustering(object):
-    distance_limit = 5
+    distance_limit = 8
     coordinate_accuracy = 0.01
     buffer_size = 0.02
     coordinate_precision = 4
