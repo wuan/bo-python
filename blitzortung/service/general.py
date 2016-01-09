409b48b15528cf2359a55c0005be1741ba949ca1
Andreas Wuerl <andi@tryb.de>
2016-01-09 21:18:17 +0000

    fix minute offset sign

--- blitzortung/service/general.py
+++ blitzortung/service/general.py
@@ -28,7 +28,7 @@ def create_time_interval(minute_length, minute_offset):
     end_time = datetime.datetime.utcnow()
     end_time = end_time.replace(tzinfo=pytz.UTC)
     end_time = end_time.replace(microsecond=0)
-    end_time += datetime.timedelta(minutes=-minute_offset)
+    end_time += datetime.timedelta(minutes=minute_offset)
     start_time = end_time - datetime.timedelta(minutes=minute_length)
     return blitzortung.db.query.TimeInterval(start_time, end_time)
 
