4d76969f1c5f7536cbb8c1c5277bd14b6cf16c8e
Andreas Würl <andi@tryb.de>
2025-01-22 07:16:15 +0100

    initial add

---
+++ blitzortung/console/db.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env pypy3
+# -*- coding: utf8 -*-
+
+"""
+Copyright (C) 2011-2014 Andreas Würl
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from __future__ import print_function
+
+import datetime
+import math
+import sys
+from optparse import OptionParser
+
+from zoneinfo import ZoneInfo
+
+import shapely.wkt
+
+import blitzortung.config
+import blitzortung.geom
+import blitzortung.util
+import blitzortung.db.query
+
+DATE_FORMAT = '%Y%m%d'
+TIME_FORMAT = '%H%M'
+SECONDS_FORMAT = '%S'
+
+
+def parse_time(date_string, time_string, description, is_end_time=False):
+    try:
+        has_seconds = len(time_string) > 4
+        tmp_date = datetime.datetime.strptime(date_string, DATE_FORMAT).date()
+        tmp_time = datetime.datetime.strptime(time_string,
+                                              TIME_FORMAT + (SECONDS_FORMAT if has_seconds else '')).time()
+        parsed_time = datetime.datetime.combine(tmp_date, tmp_time).replace(tzinfo=datetime.timezone.utc)
+        if is_end_time:
+            parsed_time += (datetime.timedelta(seconds=1) if has_seconds else datetime.timedelta(minutes=1))
+        return parsed_time
+    except Exception:
+        print("parse error in %s: '%s %s'" % description, date_string, time_string)
+        sys.exit(5)
+
+
+def prepare_grid_if_applicable(options, area):
+    if options.grid is not None or options.xgrid is not None or options.ygrid is not None:
+
+        grid_x = 1.0
+        grid_y = 1.0
+
+        if options.grid is not None:
+            grid_x = options.grid
+            grid_y = options.grid
+
+        if options.xgrid is not None:
+            grid_x = options.xgrid
+            if options.ygrid is None and options.grid is None:
+                grid_y = grid_x
+
+        if options.ygrid is not None:
+            grid_y = options.ygrid
+            if options.xgrid is None and options.grid is None:
+                grid_x = grid_y
+
+        if options.area is None:
+            print("grid options requires declaration of envelope area")
+            sys.exit(1)
+        env = area.envelope.bounds
+        return blitzortung.geom.Grid(env[0], env[2], env[1], env[3], grid_x, grid_y, options.srid)
+
+
+def run():
+    tz = datetime.timezone.utc
+    end_time = datetime.datetime.utcnow().replace(tzinfo=tz)
+    start_time = end_time - datetime.timedelta(hours=1)
+    end_time -= datetime.timedelta(minutes=1)
+
+    parser = OptionParser()
+
+    parser.add_option("--startdate", dest="startdate", default="default",
+                      help="start date for data retrieval", type="string")
+
+    parser.add_option("--starttime", dest="starttime", default="default",
+                      help="start time for data retrieval", type="string")
+
+    parser.add_option("--enddate", dest="enddate", default="default",
+                      help="end date for data retrieval", type="string")
+
+    parser.add_option("--endtime", dest="endtime", default="default",
+                      help="end time for data retrieval", type="string")
+
+    parser.add_option("--area", dest="area",
+                      help="area for which strikes are selected", type="string")
+
+    parser.add_option("--tz", dest="tz", default=str(tz),
+                      help="used timezone", type="string")
+
+    parser.add_option("--useenv", dest="useenv", default=False, action="store_true",
+                      help="use envelope of given area for query")
+
+    parser.add_option("--srid", dest="srid", default=blitzortung.geom.Geometry.default_srid,
+                      help="srid for query area and results", type="int")
+
+    parser.add_option("--precision", dest="precision", default=4,
+                      help="precision of coordinates", type="int")
+
+    parser.add_option("--grid", dest="grid",
+                      help="grid width", type="float")
+
+    parser.add_option("--x-grid", dest="xgrid",
+                      help="grid x width", type="float")
+
+    parser.add_option("--y-grid", dest="ygrid",
+                      help="grid y width", type="float")
+
+    parser.add_option("--map", dest="map", action="store_true",
+                      help="show ascii map instead of numerical grid")
+
+    (options, args) = parser.parse_args()
+
+    try:
+        tz = ZoneInfo(options.tz)
+    except:
+        print('parse error in timezone "' + options.tz + '"')
+        sys.exit(1)
+
+    start_time = start_time.astimezone(tz)
+    end_time = end_time.astimezone(tz)
+
+    if options.startdate == 'default':
+        options.startdate = start_time.strftime(DATE_FORMAT)
+    if options.starttime == 'default':
+        options.starttime = start_time.strftime(TIME_FORMAT)
+    non_default_end = options.enddate != 'default' or options.endtime != 'default'
+    if options.enddate == 'default':
+        options.enddate = end_time.strftime(DATE_FORMAT)
+    if options.endtime == 'default':
+        options.endtime = end_time.strftime(TIME_FORMAT)
+
+    start_time = parse_time(options.startdate, options.starttime, "starttime")
+    end_time = parse_time(options.enddate, options.endtime, "endtime", is_end_time=True) if non_default_end else None
+
+    area = None
+    if options.area:
+        try:
+            area = shapely.wkt.loads(options.area)
+        except:
+            print('parse error in area "' + options.area + '"')
+
+        if options.useenv:
+            area = area.envelope
+
+    # open strike database
+    strike_db = blitzortung.db.strike()
+
+    # set data parameters
+    strike_db.set_srid(options.srid)
+    strike_db.set_timezone(tz)
+
+    # set time range
+    time_interval = blitzortung.db.query.TimeInterval(start_time, end_time)
+
+    # set query parameters
+    limit = 5
+    order = 'timestamp'
+
+    grid = prepare_grid_if_applicable(options, area)
+
+    # start timer for database select
+    timer = blitzortung.util.Timer()
+
+    if grid:
+        grid_result = strike_db.select_grid(grid, time_interval=time_interval)
+
+        select_time = timer.lap()
+
+        print(grid_result.to_map() if options.map else grid_result.to_arcgrid())
+        sys.stderr.write('received grid data in %.3f seconds\n' % (select_time))
+
+    else:
+        strikes = strike_db.select(time_interval=time_interval, geometry=area, order=order)
+
+        precision_factor = math.pow(10.0, options.precision)
+
+        strike_count = 0
+        for strike in strikes:
+            location_x = round(strike.x * precision_factor) / precision_factor
+            location_y = round(strike.y * precision_factor) / precision_factor
+            strike_count += 1
+            print(str(strike))
+
+        select_time = timer.lap()
+
+        sys.stderr.write('received %d strikes in %.3f seconds\n' % (strike_count, select_time))
---
+++ blitzortung/console/import.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env pypy3
+# -*- coding: utf8 -*-
+
+import datetime
+import logging
+import os
+import time
+from optparse import OptionParser
+
+import requests
+import statsd
+
+import blitzortung.dataimport
+import blitzortung.db
+import blitzortung.logger
+import stopit
+
+from blitzortung.data import Timestamp
+
+from blitzortung_srv.lock import LockWithTimeout, FailedToAcquireException
+
+logger = logging.getLogger(os.path.basename(__file__))
+blitzortung.set_parent_logger(logger)
+blitzortung.add_log_handler(blitzortung.logger.create_console_handler())
+
+statsd_client = statsd.StatsClient('localhost', 8125, prefix='org.blitzortung.import')
+
+
+def timestamp_is_newer_than(timestamp, latest_time):
+    if not latest_time:
+        return True
+    return timestamp and timestamp > latest_time and timestamp - latest_time != datetime.timedelta()
+
+
+def import_strikes_for(region, start_time):
+    logger.debug("work on region %d", region)
+    strike_db = blitzortung.db.strike()
+    latest_time = strike_db.get_latest_time(region)
+    logger.debug("latest time for region %d: %s", region, latest_time)
+    if not latest_time:
+        latest_time = start_time
+
+    reference_time = time.time()
+    strike_source = blitzortung.dataimport.strikes()
+    strikes = strike_source.get_strikes_since(latest_time, region=region)
+    query_time = time.time()
+
+    strike_group_size = 10000
+    strike_count = 0
+    global_start_time = start_time = time.time()
+    for strike in strikes:
+        strike_db.insert(strike, region)
+
+        strike_count += 1
+        if strike_count % strike_group_size == 0:
+            strike_db.commit()
+            logger.info("commit #{} ({:.1f}/s) @{} for region {}".format(
+                strike_count, strike_group_size / (time.time() - start_time), strike.timestamp, region))
+            start_time = time.time()
+
+    if strike_count > 0:
+        strike_db.commit()
+
+    insert_time = time.time()
+    stat_name = "strikes.%d" % region
+    statsd_client.incr(stat_name)
+    statsd_client.gauge(stat_name + ".count", strike_count)
+    statsd_client.timing(stat_name + ".get", max(1, int((query_time - reference_time) * 1000)))
+    statsd_client.timing(stat_name + ".insert", max(1, int((insert_time - query_time) * 1000)))
+
+    logger.info("imported {} strikes ({:.1f}/s) for region {}".format(
+        strike_count, strike_count / (time.time() - global_start_time), region))
+
+
+def import_strikes(regions, start_time):
+    # TODO add file based lock around import call
+
+    error_count = 0
+    for region in regions:
+        for retry in range(5):
+            try:
+                with stopit.SignalTimeout(300) as timeout_ctx:
+                    import_strikes_for(region, start_time)
+                break
+            except (requests.exceptions.ConnectionError, stopit.TimeoutException):
+                logger.info('import failed: retry {} region {}'.format(retry, region))
+                error_count += 1
+                time.sleep(2)
+                continue
+    statsd_client.gauge("strikes.error_count", error_count)
+
+
+def import_station_info_for(region):
+    # TODO add file based lock around import call
+
+    imported_station_count = 0
+
+    station_source = blitzortung.dataimport.stations()
+
+    current_stations = station_source.get_stations(region=region)
+
+    if current_stations:
+        station_db = blitzortung.db.station()
+        station_offline_db = blitzortung.db.station_offline()
+
+        stations = station_db.select(region=region)
+        stations_by_number = {}
+        for station in stations:
+            stations_by_number[station.number] = station
+
+        stations_offline = station_offline_db.select(region=region)
+        stations_offline_by_number = {}
+        for station_offline in stations_offline:
+            stations_offline_by_number[station_offline.number] = station_offline
+
+        now = datetime.datetime.utcnow()
+        offline_limit_time = now - datetime.timedelta(minutes=30)
+        offline_limit_time = offline_limit_time.replace(tzinfo=datetime.timezone.utc)
+
+        for station in current_stations:
+            if station.is_valid:
+                imported_station_count += 1
+                if station.number not in stations_by_number or \
+                        station.number in stations_by_number and \
+                        station.name == stations_by_number[
+                    station.number].name and \
+                        station != stations_by_number[station.number] and \
+                        timestamp_is_newer_than(station.timestamp,
+                                                stations_by_number[station.number].timestamp):
+                    station_db.insert(station, region)
+
+                if station.timestamp is None or station.timestamp >= offline_limit_time:
+                    if station.number in stations_offline_by_number:
+                        station_offline = stations_offline_by_number[station.number]
+                        station_offline.end = station.timestamp
+                        station_offline_db.update(station_offline, region)
+                else:
+                    if station.number not in stations_offline_by_number and station.timestamp.date().year > 2000:
+                        station_offline = blitzortung.data.StationOffline(0, station.number,
+                                                                          station.timestamp)
+                        station_offline_db.insert(station_offline, region)
+            else:
+                logger.debug("INVALID: %s", station)
+
+        station_db.commit()
+        station_offline_db.commit()
+
+    logger.info("imported %d stations for region %d" % (imported_station_count, region))
+
+
+def import_station_info(regions):
+    # TODO add file based lock around import call
+
+    for region in regions:
+        import_station_info_for(region)
+
+
+def run():
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="verbose output")
+    parser.add_option("-d", "--debug", dest="debug", action="store_true", help="debug output")
+    parser.add_option("--startdate", dest="startdate", default=None, help="import start date")
+
+    (options, args) = parser.parse_args()
+
+    lock = LockWithTimeout('/tmp/.bo-import.lock')
+
+    try:
+        with lock.locked(10):
+            if options.debug:
+                blitzortung.set_log_level(logging.DEBUG)
+            elif options.verbose:
+                blitzortung.set_log_level(logging.INFO)
+            start_time = Timestamp(datetime.datetime.strptime(options.startdate, "%Y%m%d").replace(
+                tzinfo=datetime.timezone.utc)) if options.startdate else None
+
+            regions = [1, 2, 3, 4, 5, 6, 7]
+            import_strikes(regions=regions, start_time=start_time)
+            # import_station_info(regions)
+    except FailedToAcquireException:
+        logger.warning("could not acquire lock")
