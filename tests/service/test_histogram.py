851f0d445e12c22c755397334aea02ddee631e6b
Andreas Würl <andi@tryb.de>
2025-05-03 13:43:40 +0200

    add test

--- tests/service/test_histogram.py
+++ tests/service/test_histogram.py
@@ -16,24 +16,32 @@ def connection():
     return Mock(name='connection')
 
 
-@pytest.fixture
-def uut(query_builder):
-    return HistogramQuery(query_builder)
+class TestHistogramQuery:
 
+    @pytest.fixture
+    def uut(self, query_builder):
+        return HistogramQuery(query_builder)
 
-def test_create(uut, query_builder, connection):
+    def test_create(self, uut, query_builder, connection):
+        result = uut.create(connection, 30, 0)
 
-    result = uut.create(connection, 30, 0)
+        query_builder.histogram_query.assert_called_once_with("strikes", 30, 0, 5, None, None)
 
-    query_builder.histogram_query.assert_called_once_with("strikes", 30, 0, 5, None, None)
+        query = query_builder.histogram_query.return_value
+        assert connection.runQuery.call_args == call(str(query), query.get_parameters.return_value)
+        assert result == connection.runQuery.return_value
 
-    query = query_builder.histogram_query.return_value
-    assert connection.runQuery.call_args == call(str(query), query.get_parameters.return_value)
-    assert result == connection.runQuery.return_value
+        assert result.addCallback.call_args.args == (uut.build_result,)
+        assert result.addCallback.call_args.kwargs["minutes"] == 30
+        assert result.addCallback.call_args.kwargs["bin_size"] == 5
+        now = time.time()
+        assert result.addCallback.call_args.kwargs["reference_time"] > now - 0.001
+        assert result.addCallback.call_args.kwargs["reference_time"] < now + 0.001
 
-    assert result.addCallback.call_args.args == (uut.build_result,)
-    assert result.addCallback.call_args.kwargs["minutes"] == 30
-    assert result.addCallback.call_args.kwargs["bin_size"] == 5
-    now = time.time()
-    assert result.addCallback.call_args.kwargs["reference_time"] > now - 0.001
-    assert result.addCallback.call_args.kwargs["reference_time"] < now + 0.001
+    def test_build_result(self, uut):
+        reference_time = time.time() - 0.5
+        query_result = [[-2, 3], [-1, 2], [0, 1]]
+
+        result = uut.build_result(query_result, 30, 5, reference_time)
+
+        assert result == [0, 0, 0, 3, 2, 1]
