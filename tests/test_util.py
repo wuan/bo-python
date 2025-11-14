26b9cb32f9f4b85f5fb0f3c48703148854f52436
Andreas Würl <andi@tryb.de>
2025-11-14 15:48:15 +0100

    clean up tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,7 @@
 
 """
 
-   Copyright 2014-2016 Andreas Würl
+   Copyright 2013-2025 Andreas Würl
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
